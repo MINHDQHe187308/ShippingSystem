f0e62f19d7ebbe78666b83f9a7edf80b43afb2b4
Dương Minh <minhduongqh0311@gmail.com>
2025-10-24 12:38:14 +0700

    Update Database

--- trunk/ASP/Migrations/20251024053141_AddApiStatusToOrder.Designer.cs
+++ trunk/ASP/Migrations/20251024053141_AddApiStatusToOrder.Designer.cs
@@ -4,6 +4,7 @@ using ASP.Models.ASPModel;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ASP.Migrations
 {
     [DbContext(typeof(ASPDbContext))]
-    partial class ASPDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251024053141_AddApiStatusToOrder")]
+    partial class AddApiStatusToOrder
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ trunk/ASP/Migrations/20251024053141_AddApiStatusToOrder.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ASP.Migrations
+{
+    public partial class AddApiStatusToOrder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<short>(
+                name: "ApiOrderStatus",
+                table: "Orders",
+                type: "smallint",
+                nullable: false,
+                defaultValue: (short)0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ApiOrderStatus",
+                table: "Orders");
+        }
+    }
+}
